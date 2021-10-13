5cac8d53fd571933c81f6391a529c2d315fc0bff
dubisdev <dubisdev@gmail.com>
2021-10-13 22:18:11 +0200

    Change TDSClient file extensioin to ts

--- src/TDSClient.ts
+++ src/TDSClient.ts
@@ -1,7 +1,7 @@
 import task from "./client_modules/task.js";
 import project from "./client_modules/project.js";
 
-const TDSClient = (apiToken) => {
+const TDSClient = (apiToken: String) => {
 	if (!apiToken) throw new Error("Missing api token");
 
 	const headers = { Authorization: `Bearer ${apiToken}` };
