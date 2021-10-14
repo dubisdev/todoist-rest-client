ff4f2474aa0284acb762dae97e54239c58883715
dubisdev <dubisdev@gmail.com>
2021-10-14 13:20:31 +0200

    Move to interfaces and extend (reuse code)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,22 +1,19 @@
-declare type AuthHeader = {
+declare interface AuthHeader {
 	Authorization: string;
-};
+}
 
-declare type UserCreatedProject = {
+declare interface UserCreatedProject {
 	name?: string;
 	parent_id?: string;
 	color?: string;
 	favorite?: boolean;
-};
+}
 
-declare type ClientCreatedProject = {
+declare interface ClientCreatedProject extends UserCreatedProject {
 	name: string;
-	parent_id?: string;
-	color?: string;
-	favorite?: boolean;
-};
+}
 
-declare type UserCreatedTask = {
+declare interface UserCreatedTask {
 	content?: string;
 	project_id?: string;
 	section_id?: string;
@@ -29,43 +26,42 @@ declare type UserCreatedTask = {
 	due_datetime?: string;
 	due_lang?: string;
 	assignee?: string;
-};
+}
 
-declare type ClientCreatedTask = {
+declare interface ClientCreatedTask extends UserCreatedTask {
 	content: string;
-	project_id?: string;
-	section_id?: string;
-	parent_id?: string;
-	order?: string;
-	label_ids?: string;
-	priority?: string;
-	due_string?: string;
-	due_date?: string;
-	due_datetime?: string;
-	due_lang?: string;
-	assignee?: string;
-};
+}
 
-declare type TDSClient = (token: string) => {
+declare interface TDSClient {
 	apiToken: string;
-	//task: TaskAPIObject;
-	project: ProjectAPIObject;
-};
+	task: TaskModule;
+	project: ProjectModule;
+}
 
-declare type TaskAPIObject = (headers: AuthHeader) => {
-	create: (task: UserCreatedTask) => void;
-};
-
-declare type ProjectAPIObject = {
+declare interface ProjectModule {
 	create: (
 		project?: UserCreatedProject | ClientCreatedProject
-	) => Promise<object>;
+	) => Promise<APIProjectObject>;
 	getAll: () => Promise<string[]>;
 	getAllJSON: () => Promise<APIProjectObject[]>;
 	get: (id: string | number) => Promise<APIProjectObject>;
-};
+}
 
-declare type APIProjectObject = {
+declare interface TaskModule {
+	create: (
+		task?: UserCreatedTask | ClientCreatedTask
+	) => Promise<APITaskObject>;
+	closeTask: (id: number | string) => Promise<void>;
+	getAll: () => Promise<string[]>;
+	getAllJSON: () => Promise<APITaskObject[]>;
+	getToday: () => Promise<string[]>;
+	getTodayJSON: () => Promise<APITaskObject[]>;
+	getAll: () => Promise<string[]>;
+	getAllJSON: () => Promise<APITaskObject[]>;
+	get: (id: string | number) => Promise<APITaskObject>;
+}
+
+declare interface APIProjectObject {
 	id: number;
 	name: string;
 	comment_count: number;
@@ -78,4 +74,28 @@ declare type APIProjectObject = {
 	team_inbox?: boolean;
 	inbox_project?: boolean;
 	url: string;
-};
+}
+
+declare interface APITaskObject {
+	assignee?: number;
+	assigner?: number;
+	comment_count: number;
+	completed: boolean;
+	content: string;
+	description: string;
+	due: {
+		date: string;
+		datetime: string;
+		recurring: boolean;
+		string: string;
+		timezone: string;
+	};
+	id: number;
+	label_ids?: number[];
+	order: number;
+	priority: number;
+	project_id: number;
+	section_id: number;
+	parent_id: number;
+	url: string;
+}
