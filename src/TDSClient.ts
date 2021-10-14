d9084528fab722698697e47ef00dc877b2afcb74
dubisdev <dubisdev@gmail.com>
2021-10-14 13:21:43 +0200

    Type TDSClient

--- src/TDSClient.ts
+++ src/TDSClient.ts
@@ -1,7 +1,7 @@
 import task from "./client_modules/task.js";
 import project from "./client_modules/project.js";
 
-const TDSClient = (apiToken: String) => {
+const TDSClientConstructor = (apiToken: string = ""): TDSClient => {
 	if (!apiToken) throw new Error("Missing api token");
 
 	const headers = { Authorization: `Bearer ${apiToken}` };
@@ -13,4 +13,4 @@ const TDSClient = (apiToken: String) => {
 	};
 };
 
-export default TDSClient;
+export default TDSClientConstructor;
