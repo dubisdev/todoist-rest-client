ff738c0c7663bce8d9e35bbce96587213cf9ec54
dubisdev <dubisdev@gmail.com>
2021-10-14 11:57:43 +0200

    Resources Classes to Typed Objects

--- src/resources/Project.ts
+++ src/resources/Project.ts
@@ -1,14 +1,10 @@
-export default class Project {
-	name? = "_No_Project_Name_Provided_";
-	parent_id?: string;
-	color?: string;
-	favorite?: boolean;
+const Project = (project: UserCreatedProject = {}): ClientCreatedProject => {
+	const { name = "_No_Project_Name_Provided_", ...restOfProperties } = project;
 
-	constructor(obj: UserCreatedProject = {}) {
-		this.name = obj.name;
-		//these are optional
-		this.parent_id = obj.parent_id;
-		this.color = obj.color;
-		this.favorite = obj.favorite;
-	}
-}
+	return {
+		name,
+		...restOfProperties,
+	};
+};
+
+export default Project;
--- src/resources/Task.ts
+++ src/resources/Task.ts
@@ -1,46 +1,10 @@
-interface UserCreatedTask {
-	content: string;
-	project_id?: string;
-	section_id?: string;
-	parent_id?: string;
-	order?: string;
-	label_ids?: string;
-	priority?: string;
-	due_string?: string;
-	due_date?: string;
-	due_datetime?: string;
-	due_lang?: string;
-	assignee?: string;
-}
+const Task = (task: UserCreatedTask = {}): ClientCreatedTask => {
+	const { content = "_No_Project_Name_Provided_", ...restOfProperties } = task;
 
-export default class Task {
-	conten = "_NO_CONTENT_";
-	content: string;
-	project_id?: string;
-	section_id?: string;
-	parent_id?: string;
-	order?: string;
-	label_ids?: string;
-	priority?: string;
-	due_string?: string;
-	due_date?: string;
-	due_datetime?: string;
-	due_lang?: string;
-	assignee?: string;
+	return {
+		content,
+		...restOfProperties,
+	};
+};
 
-	constructor(obj: UserCreatedTask) {
-		this.content = obj.content;
-		//these are optional
-		this.project_id = obj.project_id;
-		this.section_id = obj.section_id;
-		this.parent_id = obj.parent_id;
-		this.order = obj.order;
-		this.label_ids = obj.label_ids;
-		this.priority = obj.priority;
-		this.due_string = obj.due_string;
-		this.due_date = obj.due_date;
-		this.due_datetime = obj.due_datetime;
-		this.due_lang = obj.due_lang;
-		this.assignee = obj.assignee;
-	}
-}
+export default Task;
