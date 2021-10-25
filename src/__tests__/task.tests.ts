e4be10584f8488f64382aec0f1ca470b11275984
dubisdev <dubisdev@gmail.com>
2021-10-26 00:18:04 +0200

    feat: add Task tests

--- src/__tests__/task.tests.ts
+++ src/__tests__/task.tests.ts
@@ -64,11 +64,80 @@ describe("API Tasks Functions", () => {
 		expect(errorResponse.status).toBe(404);
 	});
 
-	// test("Get All Active Tasks JSON", async () => {
-	//TODO
-	// });
+	// no active tasks now
+	test("Create Two Tasks", async () => {
+		await myClient.task.create({ content: "First task" });
+		await myClient.task.create({ content: "Second task" });
+	});
 
-	// test("Get All Active Tasks Names", async () => {
-	//TODO
-	// });
+	test("Get All Active Tasks JSON", async () => {
+		const responseTasks = await myClient.task.getAllJSON();
+		const firstTaskExists = responseTasks.some(
+			(taskObj) => taskObj.content === "First task"
+		);
+		const secondTaskExists = responseTasks.some(
+			(taskObj) => taskObj.content === "Second task"
+		);
+
+		expect(responseTasks.length).toBe(2);
+		expect(typeof responseTasks[0]).toBe("object");
+
+		expect(firstTaskExists).toBe(true);
+		expect(secondTaskExists).toBe(true);
+	});
+
+	test("Get All Active Tasks Names", async () => {
+		const responseTasks = await myClient.task.getAll();
+		const firstTaskExists = responseTasks.some((name) => name === "First task");
+		const secondTaskExists = responseTasks.some(
+			(name) => name === "Second task"
+		);
+
+		expect(responseTasks.length).toBe(2);
+		expect(typeof responseTasks[0]).toBe("string");
+
+		expect(firstTaskExists).toBe(true);
+		expect(secondTaskExists).toBe(true);
+	});
+
+	test("Complete All Previous Tasks", async () => {
+		let allTasksJSON = await myClient.task.getAllJSON();
+
+		for (let i = 0; i < allTasksJSON.length; ++i) {
+			await myClient.task.closeTask(allTasksJSON[i].id);
+		}
+
+		const responseTasks = await myClient.task.getAll();
+
+		expect(responseTasks.length).toBe(0);
+	});
+
+	// no active tasks now
+	/* Not working now because of #19
+	test("Create Two Tasks For Today", async () => {
+		// create tasks in today inbox
+		const due_info = {
+			due_lang: "en",
+			due_string: "today",
+		};
+		await myClient.task.create({ content: "First task", ...due_info });
+		await myClient.task.create({ content: "Second task", ...due_info });
+
+		//get created tasks
+		let allTodayJSON = await myClient.task.getTodayJSON();
+		const firstTaskExists = allTodayJSON.some(
+			(taskObj) => taskObj.content === "First task"
+		);
+		const secondTaskExists = allTodayJSON.some(
+			(taskObj) => taskObj.content === "Second task"
+		);
+
+		console.log(allTodayJSON);
+
+		expect(allTodayJSON.length).toBe(2);
+		expect(typeof allTodayJSON[0]).toBe("object");
+		expect(firstTaskExists).toBe(true);
+		expect(secondTaskExists).toBe(true);
+	});
+	*/
 });
